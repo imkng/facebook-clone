86d84c5de54a11f2ec6ea609da6934073af02c57
Krishna <1india.kng@gmail.com>
2022-08-03 23:37:55 +0530

    Header is created

---
+++ components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div className="hidden lg:inline-flex">
+      <div className="flex items-center space-x-2 py-3 pl-4 hover:bg-gray-200 rounded-l-xl cursor-pointer">
+
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
